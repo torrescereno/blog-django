ea297fffc28a308b7d6ae013921c5774096e1f59
LUIS GABRIEL TORRES CERENO <ltorresc@deloitte.com>
2021-08-23 18:12:18 -0400

    add template-style form post

--- posts/forms.py
+++ posts/forms.py
@@ -1,19 +1,39 @@
 from django import forms
-from posts.models import Post, Comment
+from posts.models import Post, Comment, Category
 from tinymce.widgets import TinyMCE
-
 
 class TinyMCEWidget(TinyMCE):
     def use_required_attribute(self, *args):
         return False
 
-
 class PostForm(forms.ModelForm):
-    content = forms.CharField(widget=TinyMCEWidget(attrs={'required': False, 'cols': 30, 'rows': 10}))
+
+    CHOICE = []
+    for c in Category.objects.all():
+        CHOICE.append((c.pk, c))
+
+    title = forms.CharField(widget=forms.TextInput(attrs={
+        'class':'post__form__create__title',
+        'placeholder':'Ingrese un titulo'
+    }))
+
+    categories = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple(attrs={
+        'required': True,
+        'class':'post__form__create__categories'
+    }), choices=CHOICE)
+
+    content = forms.CharField(widget=TinyMCEWidget(mce_attrs={'content_css': "/static/css/style.css"}, attrs={
+        'required': False, 
+        'rows': '10', 
+        'placeholder': 'Ingresa el contenido del post',
+        'class': 'post__form__create__content'
+    }))
+
+    thumbnail = forms.ImageField(required=False)
 
     class Meta:
         model = Post
-        fields = ('title', 'overview', 'content', 'thumbnail', 'categories')
+        fields = ('title','categories', 'content', 'thumbnail')
 
 
 class CommentForm(forms.ModelForm):

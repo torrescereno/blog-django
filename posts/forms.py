fc09333d5ce6f66a692d0e0f0d510c96d0450e21
Luis Torres Cereño <torrescereno@gmail.com>
2022-01-15 23:23:04 -0300

    cambios de estilo

--- posts/forms.py
+++ posts/forms.py
@@ -14,36 +14,34 @@ class PostForm(forms.ModelForm):
         CHOICE.append((c.pk, c))
 
     title = forms.CharField(widget=forms.TextInput(attrs={
-        'class':'post__form__create__title',
+        'class':'form-control',
         'placeholder':'Ingrese un titulo'
     }))
 
-    categories = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple(attrs={
-        'class':'post__form__create__categories'
-    }), choices=CHOICE)
+    categories = forms.MultipleChoiceField(
+        widget=forms.CheckboxSelectMultiple(attrs={'class':'form-check-input'}),
+        choices=CHOICE)
 
     content = forms.CharField(widget=TinyMCEWidget(mce_attrs={'content_css': "/static/css/style.css"}, attrs={
         'required': False, 
         'rows': '10', 
         'placeholder': 'Ingresa el contenido del post',
-        'class': 'post__form__create__content'
+        'class': ''
     }))
 
     thumbnail = forms.ImageField(required=False, widget=forms.FileInput(attrs={
-        'class':'post__form__create__img'
+        'class':'form-control'
     }))
 
     class Meta:
         model = Post
         fields = ('title','categories', 'content', 'thumbnail')
 
-
 class CommentForm(forms.ModelForm):
     content = forms.CharField(widget=forms.Textarea(attrs={
-        'class': 'post__form__comment',
+        'class': 'form-control form-control-sm',
         'id': 'usercomment',
-        'rows': '5',
-        'style': 'resize: none',
+        'placeholder':'Ingresa un comentario...'
     }))
 
     class Meta:
--- static/js/script.js
+++ static/js/script.js
@@ -1,12 +1,2 @@
-const hamburger = document.querySelector(".nav__hamburger");
-const navMenu = document.querySelector(".nav__menu")
-const navLink = document.querySelectorAll(".nav__link");
-
-function funTogle() {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-}
-
-hamburger.addEventListener("click", funTogle)
-
-navLink.forEach(n => n.addEventListener("click", funTogle));
+const divUl = document.querySelector("#id_categories");
+divUl.classList.remove("form-check-input")

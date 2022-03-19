d06fe1265079d537310e46de80823e02ece97ce1
Luis Torres Cereño <torrescereno@gmail.com>
2022-03-19 19:39:30 -0300

    changed posts cards

--- blog/views.py
+++ blog/views.py
@@ -80,7 +80,6 @@ class PostCreateView(LoginRequiredMixin, CreateView):
 
     def form_valid(self, form):
         author = get_author(self.request.user)
-        print(form)
         if not author:
             self.autor = Author.objects.create(user=self.request.user)
             form.instance.author = self.autor
